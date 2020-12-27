117119e8de58d202461068609f6741a56161cdb3
john kampitakis <jkapitakis@zoo.gr>
2020-12-27 02:05:49 +0200

    progress

---
+++ lib/apps/chat/chat_master_ban.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class ChatMasterBanPopup extends StatefulWidget {
+  @override
+  _ChatMasterBanPopupState createState() => _ChatMasterBanPopupState();
+}
+
+class _ChatMasterBanPopupState extends State<ChatMasterBanPopup> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
