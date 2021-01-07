5ca5a28c5fa16dd93f05cfd8b46b3ba80ae988fa
john kampitakis <jkapitakis@zoo.gr>
2021-01-07 18:52:59 +0200

    fixes

--- lib/apps/home/modules/banner.dart
+++ lib/apps/home/modules/banner.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/widgets.dart';
 import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:url_launcher/url_launcher.dart';
 
-class HomeModuleBanner extends StatefulWidget{
+class HomeModuleBanner extends StatefulWidget {
   HomeModuleBanner();
   HomeModuleBannerState createState() => HomeModuleBannerState();
 }
 
-class HomeModuleBannerState extends State<HomeModuleBanner>{
+class HomeModuleBannerState extends State<HomeModuleBanner> {
   HomeModuleBannerState();
 
   final String _targetLink = "https://partners.novibet.com/redirect.aspx?pid=4005&bid=1988";
@@ -23,32 +23,26 @@ class HomeModuleBannerState extends State<HomeModuleBanner>{
       setState(() {
         _bannerContents = contents;
       });
-    }
-    else print("banner source not found");
+    } else
+      print("banner source not found");
   }
 
   @override
-  void initState(){
+  void initState() {
     loadLocalHTML();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 255,
-      child: Center(
-          child: FlatButton(
-              onPressed: () async {
-                if (await canLaunch(_targetLink)) {
-                  await launch(_targetLink);
-                } else {
-                  throw 'Could not launch $_targetLink';
-                }
-              },
-            child: HtmlWidget( _bannerContents )
-          )
-      )
-    );
+    return FlatButton(
+        onPressed: () async {
+          if (await canLaunch(_targetLink)) {
+            await launch(_targetLink);
+          } else {
+            throw 'Could not launch $_targetLink';
+          }
+        },
+        child: HtmlWidget(_bannerContents));
   }
 }
