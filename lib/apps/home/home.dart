cd3d7ed2512251aa8e87d8649f12556e9ff7001a
john kampitakis <jkapitakis@zoo.gr>
2021-01-10 21:02:06 +0200

    changes

--- lib/apps/home/home.dart
+++ lib/apps/home/home.dart
@@ -15,7 +15,12 @@ import 'package:zoo_flutter/utils/global_sizes.dart';
 
 enum ModulePositions { pos1, pos2, pos3, pos4, pos5, pos6, pos7 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   final double _maxWidth = 960;
 
   getModuleForPos(ModulePositions pos, BuildContext context) {
@@ -49,65 +54,58 @@ class Home extends StatelessWidget {
       alignment: Alignment.center,
       child: Container(
         width: _maxWidth,
-        child:  Scrollbar(
-          thickness: 5,
-          showTrackOnHover: true,
-          isAlwaysShown: true,
-          radius: Radius.circular(5),
-          child: ListView(
-            children: [
-              Container(
-                  color: Color(0xFFE3E4E8),
-                  child: Container(
-                      width: _maxWidth,
-                      child: Column(
-                        children: [
-                          Container(
-                              height: 604,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Flexible(
-                                      flex: 1,
-                                      child: Column(
-                                        children: [
-                                          getModuleForPos(ModulePositions.pos1, context),
-                                          SizedBox(height: 43),
-                                          getModuleForPos(ModulePositions.pos2, context),
-                                        ],
-                                      )),
-                                  SizedBox(
-                                    width: 20,
-                                  ),
-                                  Flexible(flex: 1, child: getModuleForPos(ModulePositions.pos3, context)),
-                                  SizedBox(
-                                    width: 20,
-                                  ),
-                                  Flexible(
-                                      flex: 1,
-                                      child: Column(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          getModuleForPos(ModulePositions.pos4, context),
-                                          getModuleForPos(ModulePositions.pos5, context),
-                                        ],
-                                      ))
-                                ],
-                              )),
-                          SizedBox(height: 20),
-                          getModuleForPos(ModulePositions.pos6, context),
-                          SizedBox(height: 10),
-                          getModuleForPos(ModulePositions.pos7, context)
-                        ],
-                      ))),
-              Container(
-                margin: EdgeInsets.only(top: 10),
-                child: FooterLinks(),
-              )
-            ],
-          )
-        )
-        ,
+        child: ListView(
+          children: [
+            Container(
+                color: Color(0xFFE3E4E8),
+                child: Container(
+                    width: _maxWidth,
+                    child: Column(
+                      children: [
+                        Container(
+                            height: 604,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Flexible(
+                                    flex: 1,
+                                    child: Column(
+                                      children: [
+                                        getModuleForPos(ModulePositions.pos1, context),
+                                        SizedBox(height: 43),
+                                        getModuleForPos(ModulePositions.pos2, context),
+                                      ],
+                                    )),
+                                SizedBox(
+                                  width: 20,
+                                ),
+                                Flexible(flex: 1, child: getModuleForPos(ModulePositions.pos3, context)),
+                                SizedBox(
+                                  width: 20,
+                                ),
+                                Flexible(
+                                    flex: 1,
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        getModuleForPos(ModulePositions.pos4, context),
+                                        getModuleForPos(ModulePositions.pos5, context),
+                                      ],
+                                    ))
+                              ],
+                            )),
+                        SizedBox(height: 20),
+                        getModuleForPos(ModulePositions.pos6, context),
+                        SizedBox(height: 10),
+                        getModuleForPos(ModulePositions.pos7, context)
+                      ],
+                    ))),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              child: FooterLinks(),
+            )
+          ],
+        ),
       ),
     );
   }
