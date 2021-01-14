b72f0b762a37ddb2d9adc02e95082b50eaa1f3a4
Kostas Chatzikokolakis <kostas@chatzi.org>
2021-01-14 17:06:18 +0200

    enable testing builds

---
+++ lib/config/testing.dart
@@ -0,0 +1,19 @@
+
+// This is the config running in production in https://www.zoo.gr/?version=flutter
+
+class Config {
+  // Full URI for RPC calls
+  static const rpcUri = "https://www.test.zoo.gr/jsonrpc/api";
+
+  // Host-only, where cgi scripts are located
+  static const cgiHost = "https://www.test.zoo.gr";
+
+  // Host-only, where photos are located
+  static const userPhotosHost = "https://img.test.zoo.gr";
+
+  // Full URI of chat server
+  static const chatUri = "https://testing-1.lazyland.biz/zoo_chat/el_GR";
+
+  // hosts-only, where zmq is located
+  static const zmqHosts = ["testing-1.lazyland.biz"];
+}
